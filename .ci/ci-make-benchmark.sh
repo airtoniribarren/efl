7fd49cd9b154d2a1ca65ec030b3c6596c123d8d5
Stefan Schmidt <s.schmidt@samsung.com>
2019-11-19 12:14:23 +0100

    give correct builddir

--- .ci/ci-make-benchmark.sh
+++ .ci/ci-make-benchmark.sh
@@ -7,9 +7,9 @@ if [ "$1" != "default" ] ; then
 fi
 travis_fold benchmark "ninja benchmark"
 if [ "$DISTRO" != "" ] ; then
-  docker exec --env EIO_MONITOR_POLL=1 $(cat $HOME/cid) ninja benchmark
+  docker exec --env EIO_MONITOR_POLL=1 $(cat $HOME/cid) ninja benchmark -C build
 else
   export PATH="/usr/local/opt/ccache/libexec:$(brew --prefix gettext)/bin:$PATH"
-  ninja benchmark
+  ninja benchmark -C build
 fi
 travis_endfold benchmark
